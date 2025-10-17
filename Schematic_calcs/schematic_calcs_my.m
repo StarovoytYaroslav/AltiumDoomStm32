de94ce0bfd95adf5d1a9592dea7d3e281b2a3e6e
y.starovoyt <y.starovoyt@devcaz.com>
2025-10-17 16:03:00 +0300

    Added Matlab calcs

---
+++ Schematic_calcs/schematic_calcs_my.m
@@ -0,0 +1,11 @@
+clc; clearvars;
+L = 22 * 10^-6; %?
+Vout = 25.6;
+Vin = 3.3;
+Vf = 0.2;
+Fs = 1.2 * 10^6;
+Ilim = 1.2;
+Ip = (L * Fs *((Vout + Vf - Vin)^-1 + (Vin)^-1))^-1;
+Efficiency = .829;
+Iout_max = Vin * (Ilim - Ip/2)*Efficiency / Vout;
+Iin_dc = Vout * Iout_max/Vin/Efficiency;
